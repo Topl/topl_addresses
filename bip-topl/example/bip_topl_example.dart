d324182b9c79df09bead70206deb4e8ca6996be0
Sterling Wells <s.wells@topl.me>
2021-08-24 10:36:47 -0400

    bip-topl 0.0.1

---
+++ bip-topl/example/bip_topl_example.dart
@@ -0,0 +1,6 @@
+import 'package:bip_topl/bip_topl.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ bip-topl/lib/src/crypto/formatting.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:fast_base58/fast_base58.dart';
+import 'package:pointycastle/src/utils.dart' as p_utils;
+
+/// This method converts bytes to an unsigned integer so that it can be used more easily by our pointycastle dependency
+BigInt bytesToUnsignedInt(Uint8List bytes) {
+  return p_utils.decodeBigIntWithSign(1, bytes);
+}
+
+/// This is a utility function that is used by the keystore to decode strings that are used in the encrypted json
+Uint8List str2ByteArray(String str, {String enc = ''}) {
+  if (enc == 'latin1') {
+    return latin1.encode(str);
+  } else {
+    return Uint8List.fromList(Base58Decode(str));
+  }
+}
