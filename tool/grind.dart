92ad17e37464d76da4e0a740702edc16312fe956
Kenneth Sterling Wells <72569701+sterlingwellscaffeine@users.noreply.github.com>
2021-08-30 10:36:08 -0400

    Updated from personal account to Organization account for creating new library versions (#27)
    Co-authored-by: Sterling Wells <s.wells@topl.me>

--- tool/grind.dart
+++ tool/grind.dart
@@ -4,7 +4,7 @@ import 'package:grinder/grinder.dart';
 void main(List<String> args) {
   pkg.name.value = 'bip-topl';
   pkg.humanName.value = 'topl';
-  pkg.githubUser.value = 'sterlingwellscaffeine';
+  pkg.githubUser.value = 'Topl';
   pkg.homebrewRepo.value = 'leoafarias/homebrew-fvm';
 
   pkg.addAllTasks();
