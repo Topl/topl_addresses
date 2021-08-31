cdee1bf7f0ebd3741cdc654bba3856092a8529b8
Sterling Wells <s.wells@topl.me>
2021-08-31 11:29:19 -0400

    NPM Release

---
+++ package/README.npm.md
@@ -0,0 +1,32 @@
+A pure JavaScript implementation of bip-topl. **Library for building Topl blockchain wallet mobile apps in Flutter. Please read the documentation for more information.**.
+
+This package is a distribution of [bip-topl][], compiled to pure JavaScript
+with no native code or external dependencies. 
+
+[bip-topl]: https://github.com/Topl/bip-topl
+
+* [Usage](#usage)
+* [API](#api)
+* [See Also](#see-also)
+
+## Usage
+
+You can install bip-topl globally using `npm install -g bip_topl` which will provide
+access to the `bip_topl` library. You can also add it to your project using
+`npm install --save-dev bip_topl`. 
+
+[See below](#api) for details on Dart bip-topl's API.
+
+## API
+
+<!-- #include ../README.md -->
+
+## See Also
+
+* [bip-topl][], from which this package is compiled, can be used as a Dart library. Running bip-topl on the Dart VM is substantially faster than running the pure JavaScript version, so this may
+  be appropriate for performance-sensitive applications. The Dart API is also
+  (currently) more user-friendly than the JavaScript API. See
+  [the Dart bip-topl README][Using Dart Bip-Topl] for details on how to use it.
+
+[Using Dart Bip-Topl]: https://github.com/Topl/bip-topl#readme
+
--- tool/grind.dart
+++ tool/grind.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:charcode/ascii.dart';
 import 'package:cli_pkg/cli_pkg.dart' as pkg;
 import 'package:grinder/grinder.dart';
+import 'package:source_span/source_span.dart';
+import 'package:path/path.dart' as p;
 
 export 'grind/sanity_check.dart';
 
@@ -22,6 +26,10 @@ void main(List<String> args) {
       '# Changes\n'
       '\n'
       '${pkg.githubReleaseNotes.defaultValue}';
+  pkg.npmPackageJson.fn = () =>
+      json.decode(File('package/package.json').readAsStringSync())
+          as Map<String, dynamic>;
+  pkg.npmReadme.fn = () => _readAndResolveMarkdown('package/README.npm.md');
   pkg.addAllTasks();
   grind(args);
 }
@@ -36,6 +44,61 @@ void format() {
       script: 'format', arguments: ['--overwrite', '--fix', '.']);
 }
 
+@Task('Installs dependencies from npm.')
+void npmInstall() =>
+    run(Platform.isWindows ? 'npm.cmd' : 'npm', arguments: ['install']);
+
 @Task('Runs the tasks that are required for running tests.')
-@Depends(format, 'pkg-standalone-dev')
+@Depends(format, 'pkg-standalone-dev', 'pkg-npm-dev', npmInstall)
 void beforeTest() {}
+
+final _readAndResolveRegExp = RegExp(
+    r'^<!-- +#include +([^\s]+) +'
+    '"([^"\n]+)"'
+    r' +-->$',
+    multiLine: true);
+
+/// Reads a Markdown file from [path] and resolves include directives.
+///
+/// Include directives have the syntax `"<!-- #include" PATH HEADER "-->"`,
+/// which must appear on its own line. PATH is a relative file: URL to another
+/// Markdown file, and HEADER is the name of a header in that file whose
+/// contents should be included as-is.
+String _readAndResolveMarkdown(String path) => File(path)
+        .readAsStringSync()
+        .replaceAllMapped(_readAndResolveRegExp, (match) {
+      late String included;
+      try {
+        included = File(p.join(p.dirname(path), p.fromUri(match[1])))
+            .readAsStringSync();
+      } catch (error) {
+        _matchError(match, error.toString(), url: p.toUri(path));
+      }
+
+      late Match headerMatch;
+      try {
+        headerMatch = '# ${match[2]}'.allMatches(included).first;
+      } on StateError {
+        _matchError(match, 'Could not find header.', url: p.toUri(path));
+      }
+
+      var headerLevel = 0;
+      var index = headerMatch.start;
+      while (index >= 0 && included.codeUnitAt(index) == $hash) {
+        headerLevel++;
+        index--;
+      }
+
+      // The section goes until the next header of the same level, or the end
+      // of the document.
+      var sectionEnd = included.indexOf('#' * headerLevel, headerMatch.end);
+      if (sectionEnd == -1) sectionEnd = included.length;
+
+      return included.substring(headerMatch.end, sectionEnd).trim();
+    });
+
+/// Throws a nice [SourceSpanException] associated with [match].
+void _matchError(Match match, String message, {Object? url}) {
+  var file = SourceFile.fromString(match.input, url: url);
+  throw SourceSpanException(message, file.span(match.start, match.end));
+}
